22e46c148bc55addfdeaf92cde0da5bd29e72f38
aksmr <simon.abulkalam@edu.ece.fr>
2021-08-12 16:12:57 +0200

    Jour 3
    Ajout de toutes les villes !

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -8,13 +8,13 @@ const styles = {
         color: '#CCC9DC',
     },
     width: {
-        width: '45%',
+        width: '30%',
     }
 }
 
 const partZero = "👋🏽🤙🏽";
 const partOne = "This application makes it possible to know the number of citizens per city thanks to an API that provides this number in real time.";
-const partTwo = "To display the data for a city, please select a city from the drop-down list above.";
+const partTwo = "To access the data for a city, please select its region and department using the drop-down lists above.";
 const partThree = "Thank you very much!";
 
 export class SearchBar extends React.Component {
@@ -22,32 +22,34 @@ export class SearchBar extends React.Component {
         super(props);
         this.state = {
             error: null,
-            items: [],
             textWritten: [partZero, partOne, partTwo, partThree],
+            region: [],
+            dept: [],
+            city: [],
         };
     }
 
     componentDidMount() {
-        // let url = `https://geo.api.gouv.fr/communes?fields=nom,code,departement,region,population&format=json&geometry=centre`;
-        let url = "https://geo.api.gouv.fr/departements/93/communes?fields=nom,code,codesPostaux,codeDepartement,departement,codeRegion,region,population&format=json&geometry=centre";
+        let url_region = "https://geo.api.gouv.fr/regions?fields=nom,code";
 
-        fetch(url)
+        fetch(url_region)
         .then(res => res.json())
         .then(
             (result) => {
                 this.setState({
-                    items: result
+                    region: result
                 });
 
-                const groupSelect = document.getElementById('inputGroupSelect');
-                const listOfCities = this.state.items;
+                const groupSelectRegion = document.getElementById('inputGroupSelectRegion');
+                const listOfRegions = this.state.region;
                 let opt;
-                for(let index=0; index < listOfCities.length; index++) {
+
+                for(let index=0; index < listOfRegions.length; index++) {
                     opt = document.createElement("option");
-                    opt.value = listOfCities[index].nom.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-                    opt.id = listOfCities[index].nom.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-                    opt.text = listOfCities[index].nom;
-                    groupSelect.add(opt);
+                    opt.value = listOfRegions[index].nom.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+                    opt.id = listOfRegions[index].nom.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+                    opt.text = listOfRegions[index].nom;
+                    groupSelectRegion.add(opt);
                 }
             },
 
@@ -60,13 +62,109 @@ export class SearchBar extends React.Component {
     }
 
     render() {
-        const printCityName = () => {
-            var select = document.getElementById('inputGroupSelect');
-            var option = select.options[select.selectedIndex].text;
-            let informationsAboutTheCity = this.state.items.filter(item => item.nom === option)[0];
-            let informationsContainer;
+        const fillDeptNames = () => {
+            var select = document.getElementById('inputGroupSelectRegion');
+            if(select.value !== "Choose a region...") {
+                var option = select.options[select.selectedIndex].text;
+                let informationsAboutTheRegion = this.state.region.filter(item => item.nom === option)[0];
 
-            if(option !== `Choose a city...`) {
+                let url_region = `https://geo.api.gouv.fr/regions/${informationsAboutTheRegion.code}/departements?fields=nom,code`;
+
+                fetch(url_region)
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        this.setState({
+                            dept: result
+                        });
+
+                        const groupSelectDept = document.getElementById('inputGroupSelectDept');
+                        groupSelectDept.focus();
+                        let opt;
+
+                        groupSelectDept.innerText = null;
+                        opt = document.createElement("option");
+                        opt.text = `Choose a department...`;
+                        groupSelectDept.add(opt);
+
+                        document.getElementById('inputGroupSelectCity').innerText = null;
+                        opt = document.createElement("option");
+                        opt.text = `Choose a city...`;
+                        document.getElementById('inputGroupSelectCity').add(opt);
+
+                        const listOfDept = this.state.dept;
+
+                        for(let index=0; index < listOfDept.length; index++) {
+                            opt = document.createElement("option");
+                            opt.value = listOfDept[index].nom.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+                            opt.id = listOfDept[index].nom.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+                            opt.text = listOfDept[index].nom;
+                            groupSelectDept.add(opt);
+                        }
+                    },
+
+                    (error) => {
+                        this.setState({
+                            error
+                        });
+                    }
+                )
+            }
+        }
+
+        const fillCitiesNames = () => {
+            var select = document.getElementById('inputGroupSelectDept');
+            if(select.value !== "Choose a department...") {
+                var option = select.options[select.selectedIndex].text;
+                let informationsAboutTheDept = this.state.dept.filter(item => item.nom === option)[0];
+
+                let url_dept = `https://geo.api.gouv.fr/departements/${informationsAboutTheDept.code}/communes?fields=nom,code,codesPostaux,codeDepartement,departement,codeRegion,region,population&format=json&geometry=centre`;
+                
+                fetch(url_dept)
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        this.setState({
+                            city: result
+                        });
+
+                        const groupSelectCity = document.getElementById('inputGroupSelectCity');
+                        groupSelectCity.focus();
+                        let opt;
+
+                        groupSelectCity.innerText = null;
+                        opt = document.createElement("option");
+                        opt.text = `Choose a city...`;
+                        groupSelectCity.add(opt);
+
+                        const listOfCities = this.state.city;
+
+                        for(let index=0; index < listOfCities.length; index++) {
+                            opt = document.createElement("option");
+                            opt.value = listOfCities[index].nom.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+                            opt.id = listOfCities[index].nom.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+                            opt.text = listOfCities[index].nom;
+                            groupSelectCity.add(opt);
+                        }
+                    },
+
+                    (error) => {
+                        this.setState({
+                            error
+                        });
+                    }
+                )
+            }
+        }
+
+        const printCityName = () => {
+            var select = document.getElementById('inputGroupSelectCity');
+            let informationsContainer;
+            console.log(select.value)
+            if(select.value !== `Choose a city...`) {
+                var option = select.options[select.selectedIndex].text;
+                let informationsAboutTheCity = this.state.city.filter(item => item.nom === option)[0];
+
                 document.getElementById("nomVille").innerHTML = `🏡 ${option} 🌴`;
 
                 document.getElementById("codeINSEE").innerHTML = `INSEE Code ${informationsAboutTheCity.code}`;
@@ -96,7 +194,13 @@ export class SearchBar extends React.Component {
         return(
             <div>
                 <div class="input-group d-flex justify-content-around" style = {styles.selectButton}>
-                    <select style={styles.width} className="custom-select form-select-lg mb-3" id="inputGroupSelect" onChange={printCityName}>
+                    <select style={styles.width} className="custom-select form-select-lg mb-3" id="inputGroupSelectRegion" onChange={fillDeptNames}>
+                        <option selected>Choose a region...</option>
+                    </select>
+                    <select style={styles.width} className="custom-select form-select-lg mb-3" id="inputGroupSelectDept" onChange={fillCitiesNames}>
+                        <option selected>Choose a department...</option>
+                    </select>
+                    <select style={styles.width} className="custom-select form-select-lg mb-3" id="inputGroupSelectCity" onChange={printCityName}>
                         <option selected>Choose a city...</option>
                     </select>
                 </div>
